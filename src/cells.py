0e6e7e0128bf06f6842c631cc062032503fdbe88
Oleh Basystyi <basystyo@gmail.com>
2024-05-03 01:44:51 +0300

    Extended base project's structure

--- src/cells.py
+++ src/cells.py
@@ -2,6 +2,8 @@
 Map's cells
 """
 
+from map import Grid
+
 
 class Cell:
     """
@@ -19,14 +21,26 @@ class Cell:
         self.age = age
         self.threshold_age = threshold_age
         self.priority = status
-        self.coordinates = coordinates
+        self.x, self.y = coordinates
         self.colors = colors
 
     def infect(self, other: "Cell") -> None:
         """
-        Class
+        ...
         """
-        pass
+        raise NotImplementedError
+
+    def get_positions(self, map_: "Grid", delta: int = 1):
+        """
+        Get cell's neighbours that can be infected
+        """
+        pos = []
+        for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            if self.x + i in [-1, map_.n_rows] or self.y + j in [-1, map_.n_cols]:
+                continue
+            if self.priority - map_[self.x + i][self.y + j].priority == delta:
+                pos.append((self.x + i, self.y + j))
+        return pos
 
 
 class Void(Cell):
@@ -37,13 +51,26 @@ class Void(Cell):
     def __init__(self, coordinates, age=0) -> None:
         super().__init__(coordinates, age, 0, [])
 
+    def infect(self, other: Cell) -> None:
+        """
+        ...
+        """
+        return None
+
 
 class Water(Cell):
     """
     Water cell
     """
 
-    pass
+    def __init__(self, coordinates: tuple[int, int], age: int = 0) -> None:
+        super().__init__(coordinates, age, 30, 1)
+
+    def infect(self, other: Cell) -> None:
+        """
+        TODO
+        """
+        return super().infect(other)
 
 
 class Dirt(Cell):
@@ -51,7 +78,14 @@ class Dirt(Cell):
     Dirt cell
     """
 
-    pass
+    def __init__(self, coordinates: tuple[int, int], age: int = 0) -> None:
+        super().__init__(coordinates, age, 40, 2)
+
+    def infect(self, other: Cell) -> None:
+        """
+        TODO
+        """
+        return super().infect(other)
 
 
 class Grass(Cell):
@@ -59,7 +93,14 @@ class Grass(Cell):
     Grass cell
     """
 
-    pass
+    def __init__(self, coordinates: tuple[int, int], age: int = 0) -> None:
+        super().__init__(coordinates, age, 70, 3)
+
+    def infect(self, other: Cell) -> None:
+        """
+        TODO
+        """
+        return super().infect(other)
 
 
 class Stone(Cell):
@@ -67,7 +108,14 @@ class Stone(Cell):
     Stone cell
     """
 
-    pass
+    def __init__(self, coordinates: tuple[int, int], age: int = 0) -> None:
+        super().__init__(coordinates, age, 100, 4)
+
+    def infect(self, other: Cell) -> None:
+        """
+        TODO
+        """
+        return super().infect(other)
 
 
 class Sand(Cell):
@@ -75,4 +123,11 @@ class Sand(Cell):
     Sand cell
     """
 
-    pass
+    def __init__(self, coordinates: tuple[int, int], age: int = 0) -> None:
+        super().__init__(coordinates, age, 130, 5)
+
+    def infect(self, other: Cell) -> None:
+        """
+        TODO
+        """
+        return super().infect(other)
--- src/map.py
+++ src/map.py
@@ -2,10 +2,35 @@
 Map/grid class
 """
 
+from random import randint
+from cells import Void
+
 
 class Grid:
     """
     Grid class
     """
 
-    pass
+    def __init__(self, n, m) -> None:
+        self.n_rows = n
+        self.n_cols = m
+        self._map = [
+            [Void((i, j), 0) for j in range(self.n_cols)] for i in range(self.n_rows)
+        ]
+        self.set_up()
+
+    def set_up(self):
+        """
+        Set the map up
+        """
+        used = set()
+        arr = [Void]  # add other cell's states later
+        while arr:
+            new = (randint(0, self.n_rows), randint(0, self.n_cols))
+            if new in used:
+                continue
+            used.add(new)
+            self._map[new[0]][new[1]] = arr.pop()(new)
+
+    def __getitem__(self, i):
+        return self._map[i]
