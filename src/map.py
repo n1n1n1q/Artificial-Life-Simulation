4393ce0f68d7b6741c2938ba1c20681648bc7adf
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-05-03 17:15:26 +0300

    Made randint for set_up smaller by one. Added count_coeff, get_neighbours and update_grid functions

--- src/map.py
+++ src/map.py
@@ -27,7 +27,7 @@ class Grid:
         used = set()
         arr = [Void]  # add other cell's states later
         while arr:
-            new = (randint(0, self.n_rows), randint(0, self.n_cols))
+            new = (randint(0, self.n_rows-1), randint(0, self.n_cols-1))
             if new in used:
                 continue
             used.add(new)
@@ -35,3 +35,44 @@ class Grid:
 
     def __getitem__(self, i):
         return self._map[i]
+
+    def count_coeff(self, cell):
+        """
+        Count the number of neighboring cells of the same type in square 3x3
+        """
+        counter = 0
+        for neighbour in np.ravel(self._map[cell.x-1 if cell.x >= 1 else 0:cell.x+2, cell.y-1 if \
+cell.y >= 1 else 0:cell.y+2]):
+            if type(neighbour) is type(cell):
+                counter += 1
+        return counter
+
+    def get_neighbours(self, cell):
+        """
+        Get neighboring cells of a given cell from top, left, right and below
+        """
+        res = []
+        if cell.x != 0:
+            res.append(self._map[cell.x-1][cell.y])
+        if cell.x != len(self._map)-1:
+            res.append(self._map[cell.x+1][cell.y])
+        if cell.y != 0:
+            res.append(self._map[cell.x][cell.y-1])
+        if cell.y != len(self._map[0])-1:
+            res.append(self._map[cell.x][cell.y+1])
+        return res
+
+    def update_grid(self):
+        """
+        Walks through the grid and updates its' cells according to its rules
+        """
+        for row in self._map:
+            for cell in row:
+                if type(cell) in ["water", "void"]:
+                    for neighbour in self.get_neighbours(cell):
+                        cell.infect(neighbour)
+                else:
+                    coeff = self.count_coeff(cell)
+                    for neighbour in self.get_neighbours(cell):
+                        cell.infect(neighbour, coeff)
+                cell.age += 1
